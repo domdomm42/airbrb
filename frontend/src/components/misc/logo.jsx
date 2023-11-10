7a5435cb0927e22ebbb5473951906318a9f25a1a
z5363458 <z5363458@ad.unsw.edu.au>
2023-11-10 14:23:38 +1100

    change plane to cottage

--- frontend/src/components/misc/logo.jsx
+++ frontend/src/components/misc/logo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import FlightIcon from '@mui/icons-material/Flight';
+import CottageIcon from '@mui/icons-material/Cottage';
 
 const Logo = () => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', color: '#fff' }}>
-      <FlightIcon sx={{ fontSize: '2rem', marginRight: '0.5rem', marginBottom: '0.2rem' }} />
+      <CottageIcon sx={{ fontSize: '2rem', marginRight: '0.5rem', marginBottom: '0.2rem' }} />
       <Typography
         variant="h5"
         noWrap
