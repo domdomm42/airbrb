9280ab87a7529d1122673efd6ff6d88bd468f699
z5363458 <z5363458@ad.unsw.edu.au>
2023-11-11 14:34:54 +1100

    complete mylistings

--- frontend/src/components/listings/myListingsPage.jsx
+++ frontend/src/components/listings/myListingsPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -20,7 +21,6 @@ const MyListings = () => {
   const [userListings, setUserListings] = useState([]);
 
   useEffect(() => {
-    // Fetch user listings when the component mounts
     const fetchUserListings = async () => {
       const token = localStorage.getItem('token');
 
@@ -33,15 +33,24 @@ const MyListings = () => {
 
         if (response.ok) {
           const data = await response.json();
-          // Filter listings based on user email
           const userEmail = localStorage.getItem('email');
           const filteredListings = data.listings.filter(
             (listing) => listing.owner === userEmail
           );
-          console.log(filteredListings);
-          setUserListings(filteredListings);
+          // Fetch detailed information for each listing
+          const detailedListings = await Promise.all(
+            filteredListings.map(async (listing) => {
+              const res = await fetch(`http://localhost:5005/listings/${listing.id}`, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+              const detailedListing = await res.json();
+              return detailedListing;
+            })
+          );
+          setUserListings(detailedListings);
         } else {
-          // Handle error
           console.error('Error fetching user listings');
         }
       } catch (error) {
@@ -50,9 +59,8 @@ const MyListings = () => {
     };
 
     fetchUserListings();
-  }, []); // Empty dependency array ensures the effect runs once when the component mounts
-  console.log(userListings);
-  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }, []);
+
   return (
     <>
       <CssBaseline />
@@ -81,49 +89,90 @@ const MyListings = () => {
               spacing={2}
               justifyContent="center"
             >
-              <Button variant="contained">Create a new listing</Button>
+              <Button component={RouterLink} to="/createListing" variant="contained">
+                Create a new listing
+              </Button>
             </Stack>
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      Apartment
-                    </Typography>
-                    <Typography>
-                      $35
-                    </Typography>
-                    <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
-                    <Typography>(100)</Typography>
-                    <BathroomIcon name="bathroom" />
-                    <HotelIcon name="bedroom" />
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                    <Button size="small">Delete</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+            {userListings.length > 0
+              ? (
+                  userListings.map((listing) => (
+                    <Grid item key={listing.id} xs={12} sm={6} md={4}>
+                      <Card
+                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                      >
+                        <CardMedia
+                          component="div"
+                          sx={{
+                            // 16:9
+                            pt: '56.25%',
+                          }}
+                          image={listing.listing.thumbnail}
+                        />
+                        <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography variant="h5" component="h2" gutterBottom>
+                          {listing.listing.title}
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                          {listing.listing.metadata.propertyType}
+                        </Typography>
+                        <Typography variant="subtitle2" gutterBottom>
+                          ${listing.listing.price} AUD per night
+                        </Typography>
+                        <Grid container spacing={1} alignItems="flex-start" justifyContent="flex-start">
+                          <Grid item>
+                            <BathroomIcon name="bathroom" />
+                          </Grid>
+                          <Grid item>
+                            <Typography>{listing.listing.metadata.bathrooms} Bath</Typography>
+                          </Grid>
+                          <Grid item>
+                            <HotelIcon name="bedroom" />
+                          </Grid>
+                          <Grid item>
+                            <Typography>{listing.listing.metadata.bedrooms} Bed</Typography>
+                          </Grid>
+                        </Grid>
+
+                        <Grid container alignItems="center">
+                          <Grid item>
+                            <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="caption" gutterBottom>({listing.listing.reviews.length} reviews)</Typography>
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                      <CardActions>
+                        <Grid container justifyContent="space-between">
+                        <Grid item>
+                            <Button size="small" >
+                              Go Live
+                            </Button>
+                          </Grid>
+                          <Grid item>
+                            <Button size="small" >
+                              Edit
+                            </Button>
+                          </Grid>
+                          <Grid item>
+                            <Button size="small" >
+                              Delete
+                            </Button>
+                          </Grid>
+                        </Grid>
+                      </CardActions>
+                      </Card>
+                    </Grid>
+                  ))
+                )
+              : (
+                  <Typography variant="body1">No listings available.</Typography>
+                )}
           </Grid>
         </Container>
       </main>
