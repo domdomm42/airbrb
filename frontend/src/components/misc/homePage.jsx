d73a42f881045e9b72c86cd6a919e9151827389c
z5363458 <z5363458@ad.unsw.edu.au>
2023-11-13 01:43:45 +1100

    add homepage

--- frontend/src/components/misc/homePage.jsx
+++ frontend/src/components/misc/homePage.jsx
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Rating from '@mui/material/Rating';
 
 export const Home = () => {
+  // State to store the user's listings
+  const [userListings, setUserListings] = useState([]);
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const token = localStorage.getItem('token');
+
+      try {
+        const response = await fetch('http://localhost:5005/listings', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          // Fetch detailed information for each listing
+          const detailedListings = await Promise.all(
+            data.listings.map(async (listing) => {
+              const res = await fetch(`http://localhost:5005/listings/${listing.id}`, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+              const detailedListing = await res.json();
+              detailedListing.id = listing.id;
+              return detailedListing;
+            })
+          );
+          setUserListings(detailedListings);
+        } else {
+          console.error('Error fetching user listings');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchUserListings();
+  }, []);
   return (
-    <h1>Welcome to airbrb</h1>
-  )
+    <>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {userListings.length > 0
+              ? (
+                  userListings.map((listing) => (
+                    <Grid item key={listing.id} xs={12} sm={6} md={4}>
+                      <Card
+                        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                      >
+                        <CardMedia
+                          component="div"
+                          sx={{
+                            // 16:9
+                            pt: '56.25%',
+                          }}
+                          image={listing.listing.thumbnail}
+                        />
+                        <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography variant="h5" component="h2" gutterBottom>
+                          {listing.listing.title}
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                          {listing.listing.metadata.propertyType}
+                        </Typography>
+                        <Typography variant="subtitle2" gutterBottom>
+                          ${listing.listing.price} AUD per night
+                        </Typography>
+
+                        <Grid container alignItems="center">
+                          <Grid item>
+                            <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="caption" gutterBottom>({listing.listing.reviews.length} reviews)</Typography>
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                      </Card>
+                    </Grid>
+                  ))
+                )
+              : (
+                  <Typography variant="body1">No listings available.</Typography>
+                )}
+          </Grid>
+        </Container>
+      </main>
+    </>
+  );
 }
