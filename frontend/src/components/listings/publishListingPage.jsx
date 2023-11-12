d6e4be9254013496acfade832b3c4e7e7d694043
z5363458 <z5363458@ad.unsw.edu.au>
2023-11-13 01:38:40 +1100

    add stubs for publish listing

---
+++ frontend/src/components/listings/publishListingPage.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+export function PublishListing () {
+  // eslint-disable-next-line
+  const navigate = useNavigate();
+  // eslint-disable-next-line
+  const { listingid } = useParams();
+}
