1bb7d862f1461bb0d75082520862fa0a1c705eb4
Lim Oudom <z5329509@unsw.edu.au>
2023-11-16 18:07:40 +1100

    finished up booking

--- frontend/src/components/listings/listingPage.jsx
+++ frontend/src/components/listings/listingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -11,6 +11,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { differenceInCalendarDays } from 'date-fns';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+import TextField from '@mui/material/TextField';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
@@ -27,6 +35,14 @@ function ListingPage () {
   const [listingDetails, setListingDetails] = React.useState(null);
   const token = localStorage.getItem('token');
   const navigate = useNavigate();
+
+  // BOOKING STUFF
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+
+  const [bookingStart, setBookingStart] = useState(null);
+  const [bookingEnd, setBookingEnd] = useState(null);
+  const [bookingCost, setBookingCost] = useState(0);
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -66,6 +82,14 @@ function ListingPage () {
     fetchListingDetails();
   }, [listingid, token, navigate]);
 
+  useEffect(() => {
+    if (bookingStart && bookingEnd && listingDetails) {
+      const numberOfNights = differenceInCalendarDays(new Date(bookingEnd), new Date(bookingStart));
+      const totalCost = numberOfNights * listingDetails.listing.price;
+      setBookingCost(totalCost);
+    }
+  }, [bookingStart, bookingEnd, listingDetails]);
+
   if (!listingDetails) {
     // If listingDetails is null, return null or loading indicator
     return null;
@@ -80,6 +104,45 @@ function ListingPage () {
     : images.length;
 
   console.log(listingDetails);
+
+  const handleBookings = async () => {
+    try {
+      // Ensure dates are in the correct format (e.g., ISO string)
+
+      const formattedStartDate = bookingStart ? bookingStart.toISOString() : null;
+      const formattedEndDate = bookingEnd ? bookingEnd.toISOString() : null;
+      console.log(formattedStartDate);
+      console.log(formattedEndDate);
+      console.log(bookingCost);
+      console.log(token);
+
+      const response = await fetch(`http://localhost:5005/bookings/new/${listingid}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          dateRange: {
+            startDate: formattedStartDate,
+            endDate: formattedEndDate,
+          },
+          totalPrice: bookingCost,
+        }),
+      });
+
+      if (response.ok) {
+        setSnackbarMessage('Booking request sent successfully');
+        setSnackbarOpen(true);
+      } else {
+        setSnackbarMessage('Error sending booking request');
+        setSnackbarOpen(true);
+      }
+    } catch (error) {
+      setSnackbarMessage('Error sending booking request');
+      setSnackbarOpen(true);
+    }
+  };
 
   return (
     <>
@@ -202,10 +265,39 @@ function ListingPage () {
                   ))}
                 </ul>
               </Grid>
+              <Grid item xs={12} md={6}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Start Date"
+                  value={bookingStart}
+                  onChange={(date) => setBookingStart(date)}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+                <DatePicker
+                  label="End Date"
+                  value={bookingEnd}
+                  onChange={(date) => setBookingEnd(date)}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" onClick={handleBookings}>
+                Book Now
+              </Button>
+              <Typography variant="h6">
+                Total Cost: {bookingCost} AUD
+              </Typography>
+            </Grid>
             </Grid>
           </Container>
         </Box>
       </main>
+      <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={() => setSnackbarOpen(false)}>
+      <Alert onClose={() => setSnackbarOpen(false)} severity="success" sx={{ width: '100%' }}>
+        {snackbarMessage}
+      </Alert>
+    </Snackbar>
     </>
   );
 }
